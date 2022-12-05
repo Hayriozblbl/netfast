d7946139bff7cb41c6a55b2d38f69471c1e3c374
“hayriozblbl” <hayriozbulbul@gmail.com  git config --global user.email hayriozbulbul@gmail.com>
2022-12-05 11:18:50 +0300

    Git update

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -8,13 +8,50 @@
                     <div class="card-body text-center">
                         <img src="{{asset('backend/app-assets/images/pages/500.png')}}"
                             class="img-fluid align-self-center" alt="branding logo">
-                        <h1 class="font-large-2 mt-1 mb-0">Internal Server Error!</h1>
+                        <h1 class="font-large-2 mt-1 mb-0">Sizin için yenileniyoruz!</h1>
 
-                        <p class="p-3">
+                        <div class="content-info-part mb-60">
+                            <div class="row gutter-16">
+                                <div class="col-lg-4 md-mb-30">
+                                    <div class="info-item">
+                                        <div class="icon-part">
+                                            <i class="fa fa-phone"></i>
+                                        </div>
+                                        <div class="content-part">
+                                            <h4 class="title">Telefon Numarası:</h4>
+                                            <a  href="tel:{{ GeneralSiteSettings('site_phone')}}"><h6 class="text-gray">{{ trans('frontend.phone') }}: <p style="direction:ltr;">{{ GeneralSiteSettings('site_phone')}}</p></h6></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 md-mb-30">
+                                    <div class="info-item">
+                                        <div class="icon-part">
+                                            <i class="fa fa-at"></i>
+                                        </div>
+                                        <div class="content-part">
+                                            <h4 class="title">E-posta Adresi:</h4>
+                                            <a href="mailto:{{ GeneralSiteSettings('site_email')}}"><h6 class="text-gray">{{ GeneralSiteSettings('site_email')}}</h6></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="info-item">
+                                        <div class="icon-part">
+                                            <i class="fa fa-map-o"></i>
+                                        </div>
+                                        <div class="content-part">
+                                            <h4 class="title">Adres:</h4>
+                                            <p>{{ GeneralSiteSettings('site_address')}}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-                        </p>
 
-                        <a class="btn btn-primary btn-lg mt-2" href="{{route('frontend.index')}}">Back to Home</a>
+                     <a href="https://b2b.netfast.com.tr/login.aspx?ReturnUrl=%2f"> Bayi Portalına Giriş İçin Tıklayın</a>
+
+                        <p>Netfast.com.tr Yakında Sizlerleyiz.</p>
                     </div>
                 </div>
             </div>
