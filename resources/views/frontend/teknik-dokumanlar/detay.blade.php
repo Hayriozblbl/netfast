a5f458a0ff1864af1d07094fa353023e59a43283
“hayriozblbl” <hayriozbulbul@gmail.com  git config --global user.email hayriozbulbul@gmail.com>
2022-12-28 11:49:38 +0300

    Git update

--- resources/views/frontend/teknik-dokumanlar/detay.blade.php
+++ resources/views/frontend/teknik-dokumanlar/detay.blade.php
@@ -19,22 +19,29 @@
 
 
                     <div class="rs-featured style1 gray-bg4 pt-104 pb-75 md-pt-66 md-pb-35">
+
                         <div class="container">
                             <div class="sec-title text-center mb-62 md-mb-34 sm-mb-45">
                                 @foreach($categories as $cat)
                                 <h2 class="title mb-0 bottom-wave">{{$cat->category_name}}</h2>
                                 @endforeach
                             </div>
+                            <div class="sidebar-search sidebar-grid shadow mb-50">
+                                <form class="search-bar">
+                                    <input type="text" placeholder="Döküman Ara...">
+                                    <span>
+                                          <button type="submit"><i class="flaticon-search"></i></button>
+                                        </span>
+                                </form>
+                            </div>
                             <div class="row">
                                 @foreach($document as $doc)
 
                                 <div class="col-lg-12 col-sm-6 mb-37">
                                     <div class="featured-wrap">
-                                        <div class="icon-part pt-7  col-lg-1">
-                                            <img src="{{asset('frontend/assets/images/services/icons/4.png')}}" alt="">
-                                        </div>
                                         <div class="content-part col-lg-10">
-                                            <h4 class="title"><a href="{{asset('uploads/documents/')}}/{{ $doc->f_image}}">{{$doc->title}}</a></h4>
+                                             <h4 class="title"> <i class="fa fa-file-text" aria-hidden="true"></i>
+                                                 &nbsp <a href="{{asset('uploads/documents/')}}/{{ $doc->f_image}}">{{$doc->title}}</a></h4>
                                          </div>
                                     </div>
                                 </div>
